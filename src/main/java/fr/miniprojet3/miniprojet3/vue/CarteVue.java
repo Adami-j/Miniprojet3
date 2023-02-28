9a58ab809527d6de98dfe368b945833ff0de51c4
Julien ADAMI <adamij@3il.fr>
2023-02-28 11:29:30 +0100

    Adaptation de la carteVue avec un stackPane

--- src/main/java/fr/miniprojet3/miniprojet3/vue/CarteVue.java
+++ src/main/java/fr/miniprojet3/miniprojet3/vue/CarteVue.java
@@ -1,6 +1,37 @@
 package fr.miniprojet3.miniprojet3.vue;
 
-public class CarteVue {
+import fr.miniprojet3.miniprojet3.modele.Carte.Carte;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
+public class CarteVue extends StackPane {
+
+    private Carte carte;
+    private Rectangle background;
+    private ImageView icone;
+
+    public CarteVue(Carte carte){
+        this.carte = carte;
+        this.background = new Rectangle(100, 100);
+        this.background.setStroke(Color.BLACK);
+        this.icone = new ImageView(carte.getImage());
+    }
+
+
+    public void setCarteRetournee(boolean retournee) {
+        this.carte.setEstRetournee(retournee);
+        this.background.setVisible(retournee);
+        this.icone.setVisible(retournee);
+    }
+
+    public void setCarte(Carte carte) {
+        this.carte = carte;
+    }
+
+    public Carte getCarte() {
+        return carte;
+    }
 
 }
