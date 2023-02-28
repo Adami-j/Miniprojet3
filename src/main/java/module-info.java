998d7be98fb48e8b81605b32175237f3a6bcbf1b
Julien ADAMI <adamij@3il.fr>
2023-02-28 10:48:03 +0100

    Maven + pom.xml modifications

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module fr.miniprojet3.miniprojet3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.junit.jupiter.api;
 
 
     opens fr.miniprojet3.miniprojet3 to javafx.fxml;
     exports fr.miniprojet3.miniprojet3;
+
 }
