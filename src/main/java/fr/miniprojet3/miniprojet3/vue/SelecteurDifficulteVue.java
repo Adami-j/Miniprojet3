863e8b384c7af7e2b094d9282f763fc3bf71386a
juju <julien.adami@etu.iut-tlse3.fr>
2023-03-02 23:20:20 +0100

    modification de la vue des boutons de sélection de difficulté

--- src/main/java/fr/miniprojet3/miniprojet3/vue/SelecteurDifficulteVue.java
+++ src/main/java/fr/miniprojet3/miniprojet3/vue/SelecteurDifficulteVue.java
@@ -3,6 +3,9 @@ package fr.miniprojet3.miniprojet3.vue;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
 
 public class SelecteurDifficulteVue extends HBox {
 
@@ -20,24 +23,28 @@ public class SelecteurDifficulteVue extends HBox {
         Button button = new Button("Facile");
         Button button1 = new Button("Moyen");
         Button button2 = new Button("Difficile");
+        Button button3 = new Button("Charger des images");
+
         button1.setOnAction(event -> {
             button.setTextFill(Color.BLACK);
             button1.setTextFill(Color.GREEN);
             button2.setTextFill(Color.BLACK);
-            System.out.println("Moyen");
+
         });
         button2.setOnAction(event -> {
             button.setTextFill(Color.BLACK);
             button1.setTextFill(Color.BLACK);
             button2.setTextFill(Color.GREEN);
-            System.out.println("Difficile");
+
         });
         button.setOnAction(event -> {
-            System.out.println("Facile");
+
             button.setTextFill(Color.GREEN);
             button1.setTextFill(Color.BLACK);
             button2.setTextFill(Color.BLACK);
         });
+
+
         this.getChildren().add(button);
         this.getChildren().add(button1);
         this.getChildren().add(button2);
