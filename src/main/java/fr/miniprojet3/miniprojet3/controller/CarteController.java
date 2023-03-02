004f4f3464ccb9ca2b5f817c50b43604389cbb1b
Julien ADAMI <adamij@3il.fr>
2023-03-02 14:48:20 +0100

    ajout du controller de la carte

--- src/main/java/fr/miniprojet3/miniprojet3/controller/CarteController.java
+++ src/main/java/fr/miniprojet3/miniprojet3/controller/CarteController.java
@@ -1,6 +1,31 @@
 package fr.miniprojet3.miniprojet3.controller;
 
+import fr.miniprojet3.miniprojet3.modele.Carte.Carte;
+import fr.miniprojet3.miniprojet3.vue.CarteVue;
+import javafx.fxml.FXML;
+
+
 public class CarteController {
 
-    
+    private Carte carte;
+
+    public CarteController(Carte carte) {
+        this.carte = carte;
+
+    }
+
+    @FXML
+    public void onCarteClick() {
+        System.out.println("Carte cliquée, carte : " + carte.getEstRetournee());
+    }
+
+    public CarteController getCarteController() {
+        return this;
+    }
+
+    public void retournerCarte(){
+        carte.setEstRetournee(true);
+    }
+
+
 }
