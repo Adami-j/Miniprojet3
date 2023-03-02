59f97dd8a4f6818067fdae4aa86fdbc89b7db7e1
juju <julien.adami@etu.iut-tlse3.fr>
2023-03-02 23:20:04 +0100

    réalisation de la méthode getAllFiles pour récupérer tous les noms des éléments d'un fichier

--- src/main/java/fr/miniprojet3/miniprojet3/RecupDossierUtil.java
+++ src/main/java/fr/miniprojet3/miniprojet3/RecupDossierUtil.java
@@ -1,8 +1,38 @@
 package fr.miniprojet3.miniprojet3;
 
+import javafx.beans.property.SimpleStringProperty;
+
+import java.io.File;
+import java.util.ArrayList;
+
 public class RecupDossierUtil {
 
-    private String path;
+    private SimpleStringProperty path;
+
+    public static ArrayList<String> getAllFiles(String path) {
+        ArrayList<String> files = new ArrayList<String>();
+        File directory = new File(path);
+        // Vérifie si le chemin donné correspond à un répertoire
+        if (directory.isDirectory()) {
+            File[] listOfFiles = directory.listFiles();
+            // Parcourt tous les fichiers du répertoire et ajoute leurs noms à la liste
+            for (File file : listOfFiles) {
+                if (file.isFile()) {
+                    files.add(file.getName());
+                }
+            }
+        }
+        return files;
+    }
+
+    public static void main(String[] args) {
+        String path = "src/main/resources/fr/miniprojet3/miniprojet3/images/imagesCarte";
+        ArrayList<String> files = getAllFiles(path);
+        // Affiche le nom de tous les fichiers du répertoire
+        for (String file : files) {
+            System.out.println(file);
+        }
+    }
 
 
 
