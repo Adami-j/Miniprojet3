0f0c11bb6af8bff315c59d4199e23e5a483172af
juju <julien.adami@etu.iut-tlse3.fr>
2023-03-02 23:20:54 +0100

    modification de la méthode start, si deux cartes identiques, les retirer du jeu

--- src/main/java/fr/miniprojet3/miniprojet3/HelloApplication.java
+++ src/main/java/fr/miniprojet3/miniprojet3/HelloApplication.java
@@ -6,23 +6,34 @@ import fr.miniprojet3.miniprojet3.vue.CarteVue;
 import fr.miniprojet3.miniprojet3.vue.GrilleCarteVue;
 import fr.miniprojet3.miniprojet3.vue.SelecteurDifficulteVue;
 import javafx.application.Application;
-import javafx.fxml.FXML;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class HelloApplication extends Application {
 
-    private Carte carte;
-    private CarteVue carteVue;
+
+    private SimpleStringProperty path;
+
+    private CarteController carteController1;
+    private CarteController carteController2;
 
     @Override
     public void init() throws Exception {
@@ -36,14 +47,74 @@ public class HelloApplication extends Application {
             return new HelloController();
         });
 
+
+
         BorderPane borderPane = fxmlLoader.load();
         HBox buttonBox = new SelecteurDifficulteVue();
+        Button button3 = new Button("Chemin des images");
+        button3.setOnAction(event ->{
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            // Affiche le dialogue de sélection de dossier
+            File selectedDirectory = directoryChooser.showDialog(stage);
+            this.path.set(selectedDirectory.getAbsolutePath());
+        });
+        buttonBox.getChildren().add(button3);
 
         borderPane.setTop(buttonBox);
-        GridPane gridPane = new GrilleCarteVue();
+        GridPane gridPane;
+
+         gridPane = new GrilleCarteVue();
         gridPane.setVgap(10);
         gridPane.setHgap(10);
+        ObservableList<Carte> cartesCliques = FXCollections.observableArrayList(new ArrayList<Carte>());
 
+
+
+        // Ajoutez un ListChangeListener à la liste pour détecter les changements
+        cartesCliques.addListener((ListChangeListener<Carte>) change -> {
+
+            if (cartesCliques.size() == 2) {
+                Carte carte1 = cartesCliques.get(0);
+                Carte carte2 = cartesCliques.get(1);
+
+                CarteVue carteVue1 = ((GrilleCarteVue) gridPane).getCarteControllerMap().get(carte1).getCarteVue();
+                CarteVue carteVue2 = ((GrilleCarteVue) gridPane).getCarteControllerMap().get(carte2).getCarteVue();
+
+                if (carte1.equals(carte2)) {
+                    // les deux cartes sont identiques, traitement à effectuer
+
+                    ((GrilleCarteVue) gridPane).getCarteControllerMap().get(carte1).remouve();
+
+                    ((GrilleCarteVue) gridPane).getCarteControllerMap().get(carte2).remouve();
+
+                    System.out.println("identique ETAPE2");
+
+
+                } else  if(!carte1.equals(carte2)){
+                    // les deux cartes sont différentes, traitement à effectuer
+                    ((GrilleCarteVue) gridPane).getCarteControllerMap().get(carte1).dosCarte(carteVue1);
+                    System.out.println(carte1.toString()+carte2.toString());
+                    ((GrilleCarteVue) gridPane).getCarteControllerMap().get(carte2).dosCarte(carteVue2);
+                    System.out.println("Pas identique");
+
+
+
+                }
+                cartesCliques.removeAll(carte1,carte2);
+            }
+        });
+
+        EventHandler<MouseEvent> eventHandler = event -> {
+            CarteVue carteCliquee = (CarteVue) event.getSource();
+            System.out.println("Etape1");
+            cartesCliques.add(carteCliquee.getCarte());
+        };
+
+        for (int i = 0; i < gridPane.getChildren().size(); i++) {
+            CarteVue carteVue = (CarteVue) gridPane.getChildren().get(i);
+            Carte carte = carteVue.getCarte();
+            carteVue.addEventHandler(MouseEvent.MOUSE_PRESSED, eventHandler);
+        }
 
 
         borderPane.setCenter(gridPane);
